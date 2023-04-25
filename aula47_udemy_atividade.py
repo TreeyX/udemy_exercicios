62bed1bb306eadbf46d5c3bb3d162e5420d5eaf0
Vagner Michell <110797043+TreeyX@users.noreply.github.com>
2023-04-24 21:56:21 -0300

    Add files via upload

--- aula47_udemy_atividade.py
+++ aula47_udemy_atividade.py
@@ -1,12 +1,19 @@
 nome = input('Digite seu nome: ')
 idade = input('Digite sua idade: ')
 
-if nome and idade != False:
+if nome and idade:
     print(f'Seu nome é {nome}.')
-    print(f'A primeira letra do seu nome é {nome[1]}.')
+    print(f'A primeira letra do seu nome é {nome[0]}.')
+
+    if ' ' in nome:
+        print('Seu nome contém espaços')
+    else:
+        print('Seu nome não contém espaços')
+
     print(f'A ultima letra do seu nome é {nome[-1]}.')
-    print(f'Seu nome ao contrário é {nome[-1:-(len(nome)+1):-1]}.')
+    print(f'Seu nome ao contrário é {nome[::-1]}.')
     print(f'Seu nome tem {len(nome)} Caracteres.')
     print(f'Voce tem {idade} anos de idade.')
 else:
-    print('Nome e sua idade são obrigatórios!')
+    print('Nome e idade são obrigatórios!')
+    
